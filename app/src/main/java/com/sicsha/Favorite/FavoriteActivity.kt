3026409b4181fd36def4fb5214b514aacf62e46f
kiyoung Lee <eevee300@gmail.com>
2017-12-26 22:17:43 +0900

    Add Default Activity File

--- app/src/main/java/com/sicsha/Favorite/FavoriteActivity.kt
+++ app/src/main/java/com/sicsha/Favorite/FavoriteActivity.kt
@@ -1,12 +1,13 @@
-package com.sicsha
+package com.sicsha.Favorite
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.sicsha.R
 
-class MainActivity : AppCompatActivity() {
+class FavoriteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_favorite)
     }
 }
